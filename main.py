f5a37c3998f8dcd2b2f1cf71f8f5dea30dfe7e57
Ben L. <ben.leiby25@gmail.com>
2025-02-12 21:45:04 -0500

    GridEnvironment class

---
+++ main.py
@@ -0,0 +1,64 @@
+"""
+One potential way to build the environment is the following. You can initially set all the cells as unvisited. Then you
+can start from a random cell, mark it as visited and unblocked. Select a random neighbouring cell to visit that has not yet
+been visited. With 30% probability mark it as blocked. With 70% mark it as unblocked and in this case add it to the stack.
+A cell that has no unvisited neighbours is a dead-end. When at a dead-end, your algorithm must backtrack to parent nodes
+on the search tree until it reaches a cell with an unvisited neighbour, continuing the path generation by visiting this new,
+unvisited cell. If at some point your stack is empty, and you have not yet visited all the nodes, you repeat the above process
+from a node that has not been visited. This process continues until every cell has been visited.
+"""
+
+import random as rand
+import numpy as np
+
+class GridEnvironment:
+
+    def __init__(self, size):
+        self.size = size
+        self.grid = np.zeros((size, size))
+        self.start = None
+        self.goal = None
+
+    def get_neighbors(self, node):
+        neighbors = []
+        for dr, dc in [(0,1), (0,-1), (1,0), (-1,0)]:
+            nr, nc = node[0] + dr, node[1] + dc
+            if -1 < nr < self.size and -1 < nc < self.size:
+                neighbors.append((nr, nc))
+        return neighbors
+
+    def create_obstacles(self):
+
+        vis = np.full((self.size, self.size), False)
+        start = (rand.randint(0, self.size-1), rand.randint(0, self.size-1))
+        vis[start] = True
+
+        stack = []
+
+        current = start
+
+        next_node = rand.choice(self.get_neighbors(current))
+        while vis[next_node]:
+            next_node = rand.choice(self.get_neighbors(current))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
