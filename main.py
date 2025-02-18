5272cbca6777a30926f4505ac1f56555e04fcf0f
benleiby <105562274+benleiby@users.noreply.github.com>
2025-02-18 00:02:14 -0500

    code cleanup

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import random as rand
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 class GridEnvironment:
 
     def __init__(self, size):
         self.size = size
         self.grid = np.ones((size, size))
-        self.start = None
-        self.goal = None
+        self.start = (0,0)
+        self.goal = (size - 1, size - 1)
         self.create_maze()
 
     def get_neighbors(self, node):
@@ -26,12 +27,9 @@ class GridEnvironment:
                     self.grid[i,j] = 1
 
     def create_maze(self):
-        """Generate a maze using iterative DFS."""
-        # Choose a random starting cell (ensure it's on an even index)
         start = (rand.randint(0, (self.size - 1) // 2) * 2, rand.randint(0, (self.size - 1) // 2) * 2)
-        self.start = start
         stack = [start]
-        self.grid[start] = 0  # Mark the starting cell as a passage
+        self.grid[start] = 0
 
         while stack:
             current = stack.pop()
@@ -39,26 +37,32 @@ class GridEnvironment:
             unvisited_neighbors = [n for n in neighbors if self.grid[n] == 1]
 
             if unvisited_neighbors:
-                # Push the current cell back to the stack
-                stack.append(current)
 
-                # Choose a random unvisited neighbor
+                stack.append(current)
                 next_cell = rand.choice(unvisited_neighbors)
 
-                # Remove the wall between the current cell and the chosen neighbor
+                # Remove wall
                 wall_row = (current[0] + next_cell[0]) // 2
                 wall_col = (current[1] + next_cell[1]) // 2
-                self.grid[wall_row, wall_col] = 0  # Carve a passage
+                self.grid[wall_row, wall_col] = 0
 
-                # Mark the chosen cell as visited and push it to the stack
+                # Mark cell as visited
                 self.grid[next_cell] = 0
                 stack.append(next_cell)
 
-env = GridEnvironment(101)
-print(env.grid)
+    def visualize_maze(self):
+        cmap = mcolors.ListedColormap(['white', 'black', 'blue', 'green'])
+        bounds = [0, 1, 2, 3, 4]
+        norm = mcolors.BoundaryNorm(bounds, cmap.N)
 
-plt.imshow(env.grid, cmap='gray_r', origin='upper')  # Use 'gray' colormap for 0 and 1
-plt.title("Generated Grid")
-plt.xticks([])  # Remove x-axis ticks
-plt.yticks([])  # Remove y-axis ticks
-plt.show()
+        visualization_grid = np.copy(self.grid)
+        visualization_grid[self.start] = 2
+        visualization_grid[self.goal] = 3
+
+        plt.figure(figsize=(6, 6))
+        plt.imshow(visualization_grid, cmap=cmap, norm=norm)
+        plt.xticks([]), plt.yticks([])
+        plt.show()
+
+env = GridEnvironment(101)
+env.visualize_maze()
